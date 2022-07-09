576735cded9a0f311e138e9733ac6f8e2a28523c
ramseybelle <108037203+ramseybelle@users.noreply.github.com>
2022-07-08 21:12:56 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def fall_distance(t):  # function
+    g = 9.8
+    d = (1 / 2) * g * t ** 2  # determining the distance
+    return round(d, 3)  # return distance(round the value up to 3 decimal places as given in the question)
+# t=float(input())
+# dist=fall_distance(t)
+# print(dist)
